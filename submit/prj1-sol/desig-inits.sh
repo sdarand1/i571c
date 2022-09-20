3c70d5e937339e61d71310d5ee4ea23234d62818
Sahil Atul Darandale <sahilatuldarandale@Sahils-Air.lan>
2022-09-20 00:18:07 -0400

    Syntax Analyzer 1.3

--- submit/prj1-sol/desig-inits.sh
+++ submit/prj1-sol/desig-inits.sh
@@ -8,4 +8,4 @@ dir=`dirname $0`
 #use $dir/ as prefix to run any programs in this dir
 #so that this script can be run from any directory.
 
-java -cp $dir:./bin/:./bin/lib/json-20200518.jar SyntaxAnalyzer.SyntaxAnalyzer $inputs;
+java -cp $dir:./bin/:./src/lib/json-20200518.jar SyntaxAnalyzer.SyntaxAnalyzer $inputs;
